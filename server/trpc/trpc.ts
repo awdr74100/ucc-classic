d6aaa3e3ef9875ea1aa677fd4f072b607132715b
Yi-Ru Lan <a78945612385238@gmail.com>
2023-09-21 01:55:00 +0800

    Add trpc custom error formatter

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -1,7 +1,21 @@
 import { initTRPC } from '@trpc/server';
 import type { Context } from '@/server/trpc/context';
+import { ZodError } from 'zod';
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === 'BAD_REQUEST' && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
+});
 
 export const publicProcedure = t.procedure;
 export const router = t.router;
